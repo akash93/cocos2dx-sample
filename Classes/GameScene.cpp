cf9583a16d8789bba817d16b2985a100aa0433af
Akash Khan <akashkhan25@gmail.com>
2016-10-24 19:09:08 +0800

    Ball grid structure created

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -36,32 +36,32 @@ bool Game::init(){
     _screen_size = Director::getInstance()->getWinSize();
     _player1 = PlayerSprite::gameSpriteWithFile("goku_ssgss.png");
     _player2 = PlayerSprite::gameSpriteWithFile("goku_ssgss.png");
-    _player1->setPosition(Vec2(_screen_size.width*0.3,_screen_size.height - _player1->radius()*2));
-    _player2->setPosition(Vec2(_screen_size.width*0.7,_screen_size.height - _player2->radius()*2));
+    _player1->setPosition(Vec2(_screen_size.width*0.5,_screen_size.height*0.2 - _player1->radius()*2));
+    _player2->setPosition(Vec2(_screen_size.width*0.5,_screen_size.height*0.7));
     _player1->setScale(0.2,0.2);
     _player2->setScale(0.2,0.2);
-    this->addChild(_player1,1);
-    this->addChild(_player2,1);
+     this->addChild(_player1,1); 
+     this->addChild(_player2,1); 
     
-    std::vector<std::string> _sprite_names = {"red_ball.png","blue_ball.png","green_ball.png","yellow_ball.png","purple_ball.png","orange_ball.png"};
+    auto start_x = _screen_size.width*0.2;
+    auto start_y = _screen_size.height*0.4;
+    int distance_measure = 10;
+
+    std::vector<std::string> _sprite_names = {"blue_ball.png","yellow_ball.png","purple_ball.png","orange_ball.png"};
     std::vector<std::vector<BallSprite*>> ball_sprites;
-    for(int i = 0; i < 2; i++){
+    for(int i = 0; i < 3; i++){
         std::vector<BallSprite*> rows;
-        for(int j = 0; j < 2; j++){
-            int random = std::rand() % 7;
+        for(int j = 0; j < 4; j++){
+            int random = std::rand() % 4;
             BallSprite* ball_sprite = BallSprite::gameSpriteWithFile(_sprite_names.at(random).c_str());
-            ball_sprite->setPosition(Vec2(_screen_size.width*0.5 + i*ball_sprite->radius()*3,_screen_size.height - j*ball_sprite->radius()*3));
-            ball_sprite->setScale(40,40);
+            ball_sprite->setPosition(Vec2(start_x + j*ball_sprite->radius()*distance_measure,start_y + i*ball_sprite->radius()*distance_measure));
+            ball_sprite->setScale(8);
             rows.push_back(ball_sprite);
             this->addChild(ball_sprite,1);
         }
         ball_sprites.push_back(rows);
-    }
-    
-    auto bg_sprite = Sprite::create("MarioBg.png");
-    bg_sprite->setPosition(Vec2(_screen_size.width*0.5, _screen_size.height*0.5));
-    this->addChild(bg_sprite,0);
-    
+    } 
+      
     return true;
 }
 
